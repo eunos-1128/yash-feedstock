a5bd7b90ee0083ad7b5e64a41c0b008fa7d86b0e
UENO, M. <eunosium-1128@live.jp>
2025-06-17 10:47:54 +0000

    Change A2XFLAGS `--maxdepth` option from `10000` to `1000000`.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ if [[ "${target_platform}" == "osx-"* ]]; then
     export CC=$(command -v clang)
 fi
 
-export A2XFLAGS="--no-xmllint -v --xsltproc-opts=\"--maxdepth 10000\" \
+export A2XFLAGS="--no-xmllint -v --xsltproc-opts=\"--maxdepth 1000000\" \
     --xsl-file=${BUILD_PREFIX}/share/docbook-xsl/manpages/docbook.xsl"
 
 for f in "${BUILD_PREFIX}/lib/python2.7"/_sysconfigdata_*_conda*_linux_gnu.py; do

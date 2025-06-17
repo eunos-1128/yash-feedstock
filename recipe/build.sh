26eacaa066c6509e929ac82d7520150f12be474d
UENO, M. <eunosium-1128@live.jp>
2025-06-17 08:32:04 +0000

    Update build.sh and recipe.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,18 @@
-  #!/bin/bash
-
-export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+#!/bin/bash
 
 if [[ "${target_platform}" == "osx-"* ]]; then
     export CC=$(command -v clang)
 fi
 
+export A2XFLAGS="--no-xmllint -v --xsltproc-opts=\"--maxdepth 10000\" \
+    --xsl-file=${BUILD_PREFIX}/share/docbook-xsl/manpages/docbook.xsl"
+
+for f in "${BUILD_PREFIX}/lib/python2.7"/_sysconfigdata_*_conda*_linux_gnu.py; do
+    arch=${HOST%%-*}
+    cp -p "$f" "${BUILD_PREFIX}/lib/python2.7/_sysconfigdata_${arch}_conda_linux_gnu.py"
+    break
+done
+
 ./configure --prefix=${PREFIX}
-make install-binary -j${CPU_COUNT}
+make -j${CPU_COUNT}
+make install
